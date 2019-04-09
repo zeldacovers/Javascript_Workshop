5c488f46a595a3cc844d03f6f278ae23c05ff0b3
Meier Colin <colin.meier@blnet.lokal>
2019-04-09 08:41:16 +0200

    pushitnow

--- skript.js
+++ skript.js
@@ -49,10 +49,10 @@ $(document).ready(function () {
   }
 });
 
-$('.accordion').on('click', '.accordion-control', function(e){ // When clicked
-  e.preventDefault();                    // Prevent default action of button
-  $(this)                                // Get the element the user clicked on
-    .next('.accordion-panel')            // Select following panel 
-    .not(':animated')                    // If it is not currently animating
-    .slideToggle();                      // Use slide toggle to show or hide it
+$('.accordion').on('click', '.accordion-control', function(e){
+  e.preventDefault();                    
+  $(this)                                
+    .next('.accordion-panel')            
+    .not(':animated')                    
+    .slideToggle();                      
 });
